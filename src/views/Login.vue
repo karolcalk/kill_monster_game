60d414b94c414c6ab389594f01605b2120840494
Karol Całka <kkarol.calka@gmail.com>
2020-09-08 14:28:59 +0200

    sad

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <Form />
+  </div>
+</template>
+
+<script>
+import Form from "../components/Form";
+export default {
+  name: "Login",
+  components: {
+    Form,
+  },
+
+  data: () => ({}),
+  methods: {},
+};
+</script>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  font-family: "Press Start 2P";
+}
+
+.darkMode {
+  position: absolute;
+  right: 10px;
+  top: 10px;
+}
+</style>
